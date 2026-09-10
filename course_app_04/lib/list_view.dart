39ba3b50cd5e834e87247a04672ccc836d191676
Umut Kivanch <umutklnc1199@gmail.com>
2026-09-10 11:32:50 +0300

    Complete Lesson 11: SmartDialog and GestureDetector

--- course_app_04/lib/list_view.dart
+++ course_app_04/lib/list_view.dart
@@ -1,5 +1,6 @@
 import 'package:course_app_04/one_row_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 
 // ignore: must_be_immutable
 class OneRowListView  extends StatelessWidget{
@@ -14,15 +15,36 @@ class OneRowListView  extends StatelessWidget{
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: allStudents.length,
-      itemBuilder: (context, index) => Card(//Card widget is used to create a card-like UI for each student in the list.
-        color:allStudents[index].isBoy
-       ? Colors.blue.shade200
-       : Colors.red.shade200,
-       child: ListTile(
-        title:Text(allStudents[index].name),
-        leading: CircleAvatar(child: Text(index.toString()),),
-       ),  
-    ),
+      itemBuilder: (context, index) => GestureDetector(//GestureDetector widget is used to detect user interactions with the card widget.
+        onLongPress: (){
+          debugPrint("Card Widget has been clicked=> ${allStudents[index].name}");
+        },
+        child:Card(//Card widget is used to create a card-like UI for each student in the list.
+          color:allStudents[index].isBoy
+          ? Colors.blue.shade200
+          : Colors.red.shade200,
+          child: ListTile(
+            onTap: () {
+              debugPrint("ListTile Widget has been clicked=> ${allStudents[index].name}");
+              SmartDialog.show(builder: (context) {//SmartDialog widget is used to show a dialog box when the ListTile widget is clicked.
+                return Container(
+                  height: MediaQuery.of(context).size.height*0.4,//height of the dialog box is 40% of the screen height.
+                  width: MediaQuery.of(context).size.width*0.8,//width of the dialog box is 80% of the screen width.
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  alignment: Alignment.center,
+                  child:
+                  Text("ListTile has been clicked=> ${allStudents[index].name}", style: TextStyle(color: Colors.white)),
+                  );
+                }); 
+            },
+            title:Text(allStudents[index].name),
+            leading: CircleAvatar(child: Text(index.toString()),)
+            ),  
+        ),
+      ),
   );}
 
   ListView listView() {//ListView is a scrollable list of widgets arranged linearly.
--- course_app_04/lib/main.dart
+++ course_app_04/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:course_app_04/list_view.dart';
 // ignore: unused_import
 import 'package:course_app_04/one_row_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 
 void main() {
   runApp(const MainApp());
@@ -22,11 +23,17 @@ class MainApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false, //to remove debug banner
       title: "Material App",
+      navigatorObservers: [FlutterSmartDialog.observer],
+      builder: FlutterSmartDialog.init(),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('List and Data Processes'),
         ),
-        body: CardListTile(),//CardListTile(),//OneRowListView()
+        body: OneRowListView(), // 50 student list with GestureDetector and SmartDialog on tap
+        //body: OneRowListView().listView(), // scrollable ListView with OneRowListTile repeated twice
+        //body: OneRowListView().singleChildScrollView(), // SingleChildScrollView with one OneRowListTile
+        //body: CardListTile(), // OneRowListTile constrained inside a Card via Expanded, no scrolling
+        
       ),
     );
   }
